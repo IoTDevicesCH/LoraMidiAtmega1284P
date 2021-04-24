8a316db02dfb684b6753d7e0c66fcf31244ffed4
IoTDevicesCH <79118307+IoTDevicesCH@users.noreply.github.com>
2021-04-24 15:29:43 +0200

    updated TX_INTERVAL to 120s

--- examples/ttn-otaa-sleepWDT-idle-while-waiting/ttn-otaa-sleepWDT-idle-while-waiting.ino
+++ examples/ttn-otaa-sleepWDT-idle-while-waiting/ttn-otaa-sleepWDT-idle-while-waiting.ino
@@ -78,7 +78,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 16;
+const unsigned TX_INTERVAL = 120;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
